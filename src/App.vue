5e26bd8d4d2cfafbe70f971bd6e6edfe653aad42
Stepanov Mikhail <volodya-stepanov-68@gmail.ru>
2025-05-11 16:23:48 +0300

    feat: добавление библиотек для визуализации данных и обновление структуры приложения

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import AppFooter from './components/AppFooter.vue'
 </script>
 
 <template>
-  <RouterView />
+  <div class="app-wrapper">
+    <main class="app-content">
+      <RouterView />
+    </main>
+    <AppFooter />
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.app-wrapper {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+.app-content {
+  flex: 1;
+  padding-bottom: 2.5rem;
+}
+</style>
